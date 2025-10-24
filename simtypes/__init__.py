8b415ac6d0e9fac40007f7d3c2b126aff041b3f4
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-10-24 12:13:56 +0300

    Add PositiveInt type with validation for positive integers

--- simtypes/__init__.py
+++ simtypes/__init__.py
@@ -1,1 +1,2 @@
 from simtypes.check import check as check  # noqa: F401
+from simtypes.types.ints.positive import PositiveInt as PositiveInt  # noqa: F401
---
+++ simtypes/types/ints/positive.py
@@ -0,0 +1,6 @@
+class PositiveIntMeta(type):
+    def __instancecheck__(cls, instance):
+        return isinstance(instance, int) and instance > 0
+
+class PositiveInt(metaclass=PositiveIntMeta):
+    pass
